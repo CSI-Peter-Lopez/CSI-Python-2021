6e62c7fdfac8ccb4ca10422e8984fc8f09528b53
CSI-Peter-Lopez <25-024@sanignacio.pr>
2021-11-01 08:55:19 -0400

    Update Peter-Lopez-Project-motion.py

--- Projects/Projectile-Motion/Peter-Lopez-Project-motion.py
+++ Projects/Projectile-Motion/Peter-Lopez-Project-motion.py
@@ -1,15 +1,23 @@
-gun = "APB"
-caliber = "9x18mm Makarov"
-amunicion = "9x18mm PM SP7 gzh"
-velocity_ms = 420
-
-Building = "Oriental Plaza"
-BuildingHeight = 209.974
-
-gravity_Ms = 9.81
-
 import math
 
-time__s = math.sqrt((2 * BuildingHeight) / gravity_Ms)
+# gun = "APB"
+# caliber = "9x18mm Makarov"
+# amunicion = "9x18mm PM SP7 gzh"
+# velocity_ms = 420
 
-print(f"The gun selected for the experiment is {gun}. Whith the caliber {caliber}, amunicion {amunicion} with a velocity of {velocity_ms}. We will shoot from the {Building} building with a height of {BuildingHeight}.")
+# Building = "Oriental Plaza"
+# BuildingHeight = 209.974 
+
+# gravity_ms = 9.81
+
+
+time_s = math.sqrt((2 * BuildingHeight) / gravity_Ms)
+
+def ProjectileFuction(gun:str, caliber:str, amunicion:str, velocity_ms: int, Buildong:str, BuildingHeight: int, gravity_ms: int):
+    time_s = math.sqrt((2 * BuildingHeight) / gravity_Ms)
+    ditance_m = (velocity_ms * time_s)
+    
+    print(f"The gun selected for the experiment is {gun}. Whith the caliber {caliber}, amunicion {amunicion} with a velocity of {velocity_ms}. We will shoot from the {Building} building with a height of {BuildingHeight}.")
+
+
+ProjecttileFunction("APB", "9x18mm Makarov", "9x18mm PM SP7 gzh", 420, "Oriental Plaza", 209.974, 9.81)
