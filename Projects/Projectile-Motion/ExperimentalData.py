41c0e24df16cb577b9434379dbba70ee6b30f059
CSI-Peter-Lopez <25-024@sanignacio.pr>
2021-12-13 09:20:39 -0400

    Proyectile-Motion

---
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -0,0 +1,9 @@
+class ExperimentalData:
+    def __init__(self, gun:str, caliber:str, ammunition:str, velocity_ms: int, Building: str, BuildingHeight: int, planet:str):
+        self.gun = gun
+        self.caliber = caliber
+        self.ammunition = ammunition
+        self.velocity_ms = velocity_ms
+        self.Building = Building
+        self.BuildingHeight = BuildingHeight
+        self.planet = planet
--- Projects/Projectile-Motion/Peter-Lopez-Project-motion.py
+++ Projects/Projectile-Motion/Peter-Lopez-Project-motion.py
@@ -1,4 +1,9 @@
 import math
+import os
+from pathlib import Path
+from ExperimentalData import ExperimentalData
+import json
+
 
 # gun = "APB"
 # caliber = "9x18mm Makarov"
@@ -6,18 +11,74 @@ import math
 # velocity_ms = 420
 
 # Building = "Oriental Plaza"
-# BuildingHeight = 209.974 
+# BuildingHeight = 209.974
 
 # gravity_ms = 9.81
 
+# Convert your variables into parameters
 
-time_s = math.sqrt((2 * BuildingHeight) / gravity_Ms)
+def ProjectileFunction(experimentalData: ExperimentalData):
 
-def ProjectileFuction(gun:str, caliber:str, amunicion:str, velocity_ms: int, Buildong:str, BuildingHeight: int, gravity_ms: int):
-    time_s = math.sqrt((2 * BuildingHeight) / gravity_Ms)
-    ditance_m = (velocity_ms * time_s)
+    planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+    g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
+
+    planet = planets.index(ExperimentalData.planet)
+    time_s = math.sqrt((2 * ExperimentalData.BuildingHeight) / g_ms2[planets])
+
+    distance_m = (ExperimentalData.velocity_ms * time_s)
+    # distance_m = (experimentalData[velocity_ms] * time_s)
     
-    print(f"The gun selected for the experiment is {gun}. Whith the caliber {caliber}, amunicion {amunicion} with a velocity of {velocity_ms}. We will shoot from the {Building} building with a height of {BuildingHeight}.")
+    print(f"The gun selected for the experiment is {ExperimentalData.gun}. Whith the caliber {ExperimentalData.caliber}, amunicion {ExperimentalData.amm4unition} with a velocity of {ExperimentalData.velocity_ms}. We will shoot from the {ExperimentalData.Building} building with a height of {ExperimentalData.BuildingHeight}.")
+    print(f"The experiment is carried out in {ExperimentalData.planet} with a gravity of {g_ms2[planet]}")
 
 
-ProjecttileFunction("APB", "9x18mm Makarov", "9x18mm PM SP7 gzh", 420, "Oriental Plaza", 209.974, 9.81)
+# Convert your script parameters into a single JSON object
+# experimentalData = {
+
+# "gun": "APB",
+# "caliber":  "9x18mm Makarov",
+# "amunicion": "9x18mm PM SP7 gzh",
+# "velocity_ms": 420,
+# "Building":  "Oriental Plaza",
+# "BuildingHeight": 209.974,
+# "gravity_ms": 9.81,
+
+# }
+
+# experimentalData = ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM SP7 gzh", 420, "Oriental Plaza", 209.974, 9.81)
+
+
+myDataSet = [
+ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM SP7 gzh", 420, "Oriental Plaza", 209.974, "Earth"),
+ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM BZhT gzh", 325, "Oriental Plaza", 209.974, "Mars"),
+ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM P gzh", 302, "Oriental Plaza", 209.974, "Venus"),
+ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM PBM gzh", 519, "Oriental Plaza", 209.974, "Saturn"),
+ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM SP8 gzh", 250, "Oriental Plaza", 209.974, "Mercury"),
+ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM RG028 gzh", 330, "Oriental Plaza", 209.974, "Jupiter"),
+ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM PSV", 280, "Oriental Plaza", 209.974, "Neptune"),
+ExperimentalData("APB", "9x18mm Makarov", "9x18mm PM Pst gzh", 298, "Oriental Plaza", 209.974, "Uranus")
+
+]
+
+for data in myDataSet:
+    print("\n--------------------------------------\n")
+    ProjectileFunction(data)
+
+# Serialization
+myOutputPath = Path(__file__).parents[0]
+myOutputFilePath = os.path.join(myOutputPath, 'Projectile-Motion.json')
+
+print(myOutputFilePath)
+
+with open(myOutputFilePath, 'w') as outfile:
+    json.dump([data.__dict__ for data in myDataSet], outfile)
+
+# Deserialization
+deserialize = open(myOutputFilePath)
+experimentJson = json.load(deserialize)
+
+for e in experimentJson:
+    print("\n--------------------------------------\n")
+    ProjectileFunction(ExperimentalData(**e))
+
+# ProjectileFuction(experimentalData)
