03afb0089e031ad199c830ae3e2c80d78639d15d
CSI-Peter-Lopez <25-024@sanignacio.pr>
2022-02-22 09:21:12 -0400

    Create Peter-Lopez-Snakegame.py
    snake-game

---
+++ Projects/Peter-Lopez-Snakegame.py
@@ -0,0 +1,94 @@
+import pygame
+import time
+import random
+pygame.init() # inicia todos los módulos de Pygame
+
+
+yellow = (255, 255, 102) #Score
+black = (0, 0, 0) #pantalla
+red = (213, 50, 80) #game over
+green = (0, 255, 0) #food
+pink = (238,18,137) #snake
+
+dis_width = 400
+dis_height = 300
+dis=pygame.display.set_mode((dis_width,dis_height))
+
+pygame.display.set_caption("Snake game by PL")
+game_over=False
+
+snake_block = 10
+snake_sped = 30
+
+clock = pygame.time.Clock()
+
+font_style =pygame.font.SysFont(None, 25)
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width/7, dis_height/2.5])
+
+def gameRestart():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width/2
+    y1 = dis_height/2
+    
+    x1_change = 0
+    y1_change = 0 
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10) *10
+    foody = round(random.randrange(0, dis_width - snake_block) / 10) *10
+
+    
+    while not game_over:
+
+        while game_close == True:
+            dis.fill(black)
+            message("YOU LOST Press Q-Quit or P-Play Again", red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type==pygame.KEYDOWN: 
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_p:
+                        gameRestart()
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT: 
+                game_over=True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                if event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                if event.key == pygame.K_UP:
+                    x1_change = 0
+                    y1_change = -snake_block
+                if event.key == pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = snake_block
+        if x1 >=dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(black)
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block]) #crea el food
+        pygame.draw.rect(dis, pink, [x1,y1, snake_block, snake_block]) # crea el snake y lo ubica en la pantalla
+        pygame.display.update()
+
+        clock.tick(snake_sped)
+
+#pygame.display.update()
+#time.sleep(5)
+    pygame.quit()
+    quit()
+
+gameRestart()
+
